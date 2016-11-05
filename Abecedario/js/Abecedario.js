4b2476316efcd9ffe31aa03c4f12f32267a6af54
jhelsin quispe garcia <jhelsin quispe garcia>
2016-11-05 11:37:42 -0500

    rgba

--- Abecedario/js/Abecedario.js
+++ Abecedario/js/Abecedario.js
@@ -1,3 +1,23 @@
-function decirVocal(var vocal){
+var audio = new Audio('Different Heaven & EH!DE - My Heart.mp3');
+
+function decirVocal(){
+	
+	audio.volume = 0.5;
+
+	audio.play();
+	
+	var play = document.getElementById("play");
+
+	//play.style.width = "0px";
+	//play.style.height = "0px";
+	//play.style.border = "0px";
+}
+function detener(){
+	
+	audio.volume = 0.5;
+
+	
+		audio.pause();
+	
 	
 }
