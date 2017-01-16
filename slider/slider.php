d0f705c48f04227c9dd9693c16d1b3b4eaec82e7
jhelsin quispe garcia <jhelsin quispe garcia>
2017-01-15 21:14:44 -0500

    rgba

---
+++ slider/slider.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>slider</title>
+	<link rel="stylesheet" type="text/css" href="slider.css">
+</head>
+<body>
+	<div class="slider">
+		<ul>
+			<li><img src="../img/textura1.jpg" width="600px" height="300px" alt=""></li>
+			<li><img src="../img/textura2.jpg" width="600px" height="300px" alt=""></li>
+			<li><img src="../img/textura3.jpg" width="600px" height="300px" alt=""></li>
+			<li><img src="../img/textura4.jpg" width="600px" height="300px" alt=""></li>
+		</ul>
+	</div>
+</body>
+</html>
