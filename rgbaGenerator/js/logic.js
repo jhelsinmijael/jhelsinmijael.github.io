25afd964c95659c9d44dd617893497079caf6941
jhelsin quispe garcia <jhelsin quispe garcia>
2017-02-25 12:02:13 -0500

    rgba

--- rgbaGenerator/js/logic.js
+++ rgbaGenerator/js/logic.js
@@ -1,34 +1,69 @@
-function pintar(color){
-	var boton = document.getElementById("boton");
-	boton.style.background = color;
-}
-			
-function pintar2(color){
-	document.getElementById("codigo2").value = document.getElementById("codigo").value;
-	var opacidad = document.getElementById("select_opacidad").value;
-	var cantidad_rojo = document.getElementById("select_rojo").value;
-	var cantidad_verde = document.getElementById("select_verde").value;
-	var cantidad_azul = document.getElementById("select_azul").value;
+$(document).ready(function() {
+	var cabecera = $("#cabecera");
+	var boton = $("#boton");
+	var body = $("#body");
+	var content1 = $("#content1");
+	var content2 = $("#content2");
+	//var content3 = $("#content3");
+	var btn_circle = $("#btn_circle");
+});
 
-				
-	var cabecera = document.getElementById("cabecera");
-	var boton = document.getElementById("boton");
+function pintar(r,g,b){
+	//only javascript form
+	//var boton = document.getElementById("boton");
+	//boton.style.background = color;
+	//JQuery form
+	var boton = $("#boton");
+	var color = "rgba("+r+","+g+","+b+",1)";
+	boton.css('background', color);
+	
+}
+
+function pintar2(color){
+	//document.getElementById("codigo2").value = document.getElementById("codigo").value;
+	var codigo = $("#codigo").val();
+	$("#codigo2").val(codigo)
+	var opacidad = $("#select_opacidad").val();
+	var cantidad_rojo = $("#select_rojo").val();
+	var cantidad_verde = $("#select_verde").val();
+	var cantidad_azul = $("#select_azul").val();
+
 	if(opacidad==10){
-	boton.style.background = "rgba("+cantidad_rojo+","+cantidad_verde+","+cantidad_azul+","+opacidad+")";
-	cabecera.style.background = "rgba("+cantidad_rojo+","+cantidad_verde+","+cantidad_azul+","+opacidad+")";
+		boton.style.background = "rgba("+cantidad_rojo+","+cantidad_verde+","+cantidad_azul+","+opacidad+")";
+		cabecera.style.background = "rgba("+cantidad_rojo+","+cantidad_verde+","+cantidad_azul+","+opacidad+")";
+		body.style.background = "rgba("+cantidad_rojo+","+cantidad_verde+","+cantidad_azul+",0.1)";
 		
-	document.getElementById("codigo").value="rgba("+cantidad_rojo+","+cantidad_verde+","+cantidad_azul+",1)";
+		btn_circle.style.background = "rgba("+cantidad_rojo+","+cantidad_verde+","+cantidad_azul+","+opacidad+")";
+
+		content1.style.borderTop = "3px solid rgba("+cantidad_rojo+","+cantidad_verde+","+cantidad_azul+","+opacidad+")";
+		content2.style.borderTop = "3px solid rgba("+cantidad_rojo+","+cantidad_verde+","+cantidad_azul+","+opacidad+")";
+		//content3.style.borderTop = "3px solid rgba("+cantidad_rojo+","+cantidad_verde+","+cantidad_azul+","+opacidad+")";
+		document.getElementById("codigo").value="rgba("+cantidad_rojo+","+cantidad_verde+","+cantidad_azul+",1)";
 	}else{
 		boton.style.background = "rgba("+cantidad_rojo+","+cantidad_verde+","+cantidad_azul+",0."+opacidad+")";
 		document.getElementById("codigo").value="rgba("+cantidad_rojo+","+cantidad_verde+","+cantidad_azul+",0."+opacidad+")";
 		cabecera.style.background = "rgba("+cantidad_rojo+","+cantidad_verde+","+cantidad_azul+",0."+opacidad+")";
+		body.style.background = "rgba("+cantidad_rojo+","+cantidad_verde+","+cantidad_azul+",0.1)";
 	}
 
 }
 
 function copiar(){
-
 	$("#codigo2").select(); // Acá se obtiene el id del boton que hemos creado antes y se le agrega un valor y luego se le sombrea con select(). Para agregar lo que se quiere copiar editas val("EDITAESTOAQUÍ")
-    document.execCommand("copy"); 
+	document.execCommand("copy"); 
 
+}
+
+function cambiarColor(){
+	var cantidad_rojo = $("#select_rojo").val();
+	var cantidad_verde = $("#select_verde").val();
+	var cantidad_azul = $("#select_azul").val();
+	btn_circle.style.background = "rgba("+cantidad_rojo+","+cantidad_verde+","+cantidad_azul+",0.9)";
+}
+
+function regresarColor(){
+	var cantidad_rojo = $("#select_rojo").val();
+	var cantidad_verde = $("#select_verde").val();
+	var cantidad_azul = $("#select_azul").val();
+	btn_circle.style.background = "rgba("+cantidad_rojo+","+cantidad_verde+","+cantidad_azul+",1)";
 }
