be40abb08719214b997a0e271646fa24c2316ddc
jhelsin quispe garcia <jhelsin quispe garcia>
2017-02-27 16:53:22 -0500

    rgba

--- Subneteador/js1/logica.js
+++ Subneteador/js1/logica.js
@@ -156,6 +156,8 @@ function definirSubred(){
 
 		var num_subred = document.getElementById("caja_numero_subred").value;
 
+		$("#mensaje").text(num_subred+' subredes creades con exito! desplazate hacia habajo');
+
 		var bits = 0;
 
 		var mask=0;
@@ -209,6 +211,8 @@ function definirSubred(){
 		var n = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24];
 
 		var num_subred = document.getElementById("caja_numero_subred").value;
+
+		$("#mensaje").text(num_subred+' subredes creades con exito! desplazate hacia habajo');
 
 		var bits = 0;
 
