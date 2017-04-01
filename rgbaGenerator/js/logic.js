68a12b35a63ce7c3288fee8c10b98ef54dba933e
jhelsin quispe garcia <jhelsin quispe garcia>
2017-04-01 13:38:22 -0500

    rgba

--- rgbaGenerator/js/logic.js
+++ rgbaGenerator/js/logic.js
@@ -7,6 +7,7 @@ $(document).ready(function() {
 	//var content3 = $("#content3");
 	var btn_circle = $("#btn_circle");
 	var header = $("#header");
+
 });
 
 function pintar(r,g,b){
@@ -14,7 +15,7 @@ function pintar(r,g,b){
 	//var boton = document.getElementById("boton");
 	//boton.style.background = color;
 	//JQuery form
-	var boton = $("#boton");
+	var boton = $("#boton");	var boton2 = $("#boton2");
 	var color = "rgba("+r+","+g+","+b+",1)";
 	boton.css('background', color);
 	
@@ -29,30 +30,33 @@ function pintar2(color){
 	var cantidad_verde = $("#select_verde").val();
 	var cantidad_azul = $("#select_azul").val();
 
+	definirColor(cantidad_rojo,cantidad_verde,cantidad_azul);
+
 	if(opacidad==10){
 		boton.style.background = "rgba("+cantidad_rojo+","+cantidad_verde+","+cantidad_azul+","+opacidad+")";
+		$("#boton2").css('border', "1px solid rgba("+cantidad_rojo+","+cantidad_verde+","+cantidad_azul+","+opacidad+")");
+		$("#boton2").css('color', "rgba("+cantidad_rojo+","+cantidad_verde+","+cantidad_azul+","+opacidad+")");
+		$("#boton2").css('background', "white");
 		cabecera.style.background = "rgba("+cantidad_rojo+","+cantidad_verde+","+cantidad_azul+","+opacidad+")";
-		body.style.background = "rgba("+cantidad_rojo/2+","+cantidad_verde/2+","+cantidad_azul/2+",0.1)";
+		body.style.background = "rgba("+(parseFloat(cantidad_rojo)/2)+","+(parseFloat(cantidad_verde)/2)+","+(parseFloat(cantidad_azul)/2)+",0.2)";
 		
 		btn_circle.style.background = "rgba("+cantidad_rojo+","+cantidad_verde+","+cantidad_azul+","+opacidad+")";
 
-		content1.style.borderTop = "3px solid rgba("+cantidad_rojo+","+cantidad_verde+","+cantidad_azul+","+opacidad+")";
-		content2.style.borderTop = "3px solid rgba("+cantidad_rojo+","+cantidad_verde+","+cantidad_azul+","+opacidad+")";
-		//content3.style.borderTop = "3px solid rgba("+cantidad_rojo+","+cantidad_verde+","+cantidad_azul+","+opacidad+")";
-		document.getElementById("codigo").value="rgba("+cantidad_rojo+","+cantidad_verde+","+cantidad_azul+",1)";
+	//content3.style.borderTop = "3px solid rgba("+cantidad_rojo+","+cantidad_verde+","+cantidad_azul+","+opacidad+")";
+	document.getElementById("codigo").value="rgba("+cantidad_rojo+","+cantidad_verde+","+cantidad_azul+",1)";
+}else{
+	boton.style.background = "rgba("+cantidad_rojo+","+cantidad_verde+","+cantidad_azul+",0."+opacidad+")";
+	document.getElementById("codigo").value="rgba("+cantidad_rojo+","+cantidad_verde+","+cantidad_azul+",0."+opacidad+")";
+	cabecera.style.background = "rgba("+cantidad_rojo+","+cantidad_verde+","+cantidad_azul+",0."+opacidad+")";
+	body.style.background = "rgba("+cantidad_rojo+","+cantidad_verde+","+cantidad_azul+",0.1)";
+	if(opacidad==3 || opacidad==2 || opacidad==1 || opacidad==0){
+
+		header.style.color = "#006699";
+
 	}else{
-		boton.style.background = "rgba("+cantidad_rojo+","+cantidad_verde+","+cantidad_azul+",0."+opacidad+")";
-		document.getElementById("codigo").value="rgba("+cantidad_rojo+","+cantidad_verde+","+cantidad_azul+",0."+opacidad+")";
-		cabecera.style.background = "rgba("+cantidad_rojo+","+cantidad_verde+","+cantidad_azul+",0."+opacidad+")";
-		body.style.background = "rgba("+cantidad_rojo+","+cantidad_verde+","+cantidad_azul+",0.1)";
-		if(opacidad==3 || opacidad==2 || opacidad==1 || opacidad==0){
-
-			header.style.color = "#006699";
-
-		}else{
-			header.style.color = "#fff";
-		}
+		header.style.color = "#fff";
 	}
+}
 
 }
 
@@ -75,3 +79,16 @@ function regresarColor(){
 	var cantidad_azul = $("#select_azul").val();
 	btn_circle.style.background = "rgba("+cantidad_rojo+","+cantidad_verde+","+cantidad_azul+",1)";
 }
+
+definirColor = function(rojo,verde,azul){
+		$("#boton2").hover(function(){
+			$(this).css('background', "rgba("+rojo+","+verde+","+azul+",1)");
+			$(this).css('color', 'white');
+
+		});
+		$("#boton2").mouseout(function(){
+			$(this).css('background', "white");
+			$(this).css('color', "rgba("+rojo+","+verde+","+azul+",1)");
+
+		});
+	}
