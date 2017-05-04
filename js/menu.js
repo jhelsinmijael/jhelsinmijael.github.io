89dfaf4b9fce22b88dc9ae320d1dbf775bd028dc
jhelsin quispe garcia <jhelsin quispe garcia>
2017-05-04 05:09:29 -0500

    rgba

--- js/menu.js
+++ js/menu.js
@@ -3,7 +3,7 @@ $("#menu-bar").click(function() {
 	if( $('#menu-bar').prop('checked') ){
 		$("#label_menu").css('transition-duration', '0.4s');
 		$("#label_menu").html('<span id="img-menu" class="glyphicon glyphicon-remove"></span>');
-		$("#label_menu").css('color', 'rgba(255,137,128,1)');
+		$("#label_menu").css('color', 'rgba(255,88,87,1)');
 		
 	}else{
 		$("#label_menu").css('transition-duration', '0.1s')
