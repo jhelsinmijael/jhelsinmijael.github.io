3e2b50e797232999146f888f2967c107370aa95d
jhelsin quispe garcia <jhelsin quispe garcia>
2017-05-04 05:06:07 -0500

    rgba

---
+++ js/menu-content.js
@@ -0,0 +1,22 @@
+$("#div_menu").html(
+	'<input type="checkbox" id="menu-bar">'+
+	'<label  id="label_menu" class="icon-menu" for="menu-bar">'+
+	'<span id="img-menu" class="glyphicon glyphicon-align-justify"></span>'+
+	'</label>'+
+	'<nav class="menu">'+
+
+	'<a href="../index.html"><span class="glyphicon glyphicon-home"></span>'+
+	'<p>Inicio</p>'+
+	'</a>'+
+	'<a href="https://www.youtube.com/channel/UCe112PzmEonMRh0AjIBAGcg?sub_confirmation=1"><span class="glyphicon glyphicon-hd-video"></span>'+
+	'<p>Canal</p>'+
+	'</a>'+
+	'<a href=""><span class="glyphicon glyphicon-book"></span>'+
+	'<p>Blog</p>'+
+	'</a>'+
+	'<a href=""><span class="glyphicon glyphicon-phone"></span>'+
+	'<p>Contacto</p>'+
+	'</a>'+
+
+	'</nav>'
+	);
--- js/menu.js
+++ js/menu.js
@@ -1,52 +1,15 @@
-$(document).ready(function(){
-	$(".item1").mouseover(function(){
-		$(".info1").html("visit youtube channel");
-		$(".info1").addClass("infoshow");
-	});
 
-	$(".item2").mouseover(function(){
-		$(".info2").html("visit facebook page");
-		$(".info2").addClass("infoshow");
-	});
+$("#menu-bar").click(function() {
+	if( $('#menu-bar').prop('checked') ){
+		$("#label_menu").css('transition-duration', '0.4s');
+		$("#label_menu").html('<span id="img-menu" class="glyphicon glyphicon-remove"></span>');
+		$("#label_menu").css('color', 'rgba(255,137,128,1)');
+		
+	}else{
+		$("#label_menu").css('transition-duration', '0.1s')
+		$("#label_menu").html('<span id="img-menu" class="glyphicon glyphicon-align-justify"></span>');
+		$("#label_menu").css('color', 'white');
+	}
 
-	$(".item3").mouseover(function(){
-		$(".info3").html("hello katherim <3");
-		$(".info3").addClass("infoshow");
-	});
-
-	$(".item4").mouseover(function(){
-		$(".info4").html("hello katherim <3");
-		$(".info4").addClass("infoshow");
-	});
-
-	$(".item5").mouseover(function(){
-		$(".info5").html("hello katherim <3");
-		$(".info5").addClass("infoshow");
-	});
-
-	$(".item1").mouseout(function(){
-		$(".info1").html("");
-		$(".info1").removeClass("infoshow");
-	});
-
-	$(".item2").mouseout(function(){
-		$(".info2").html("");
-		$(".info2").removeClass("infoshow");
-	});
-
-	$(".item3").mouseout(function(){
-		$(".info3").html("");
-		$(".info3").removeClass("infoshow");
-	});
-
-	$(".item4").mouseout(function(){
-		$(".info4").html("");
-		$(".info4").removeClass("infoshow");
-	});
-
-	$(".item5").mouseout(function(){
-		$(".info5").html("");
-		$(".info5").removeClass("infoshow");
-	});
-})
+});
 
